705f0edaad61c958c03ca914c98ee37e2e8be433
“Nishigandha <nishigandhatawlare@gmail.com>
2024-09-03 21:06:44 +0530

    Add project files.

---
+++ ECommerseWebApp.Services.CouponAPI/Controllers/CouponApiController.cs
@@ -0,0 +1,53 @@
+﻿using ECommerseWebApp.Services.CouponAPI.Data;
+using ECommerseWebApp.Services.CouponAPI.Models;
+using ECommerseWebApp.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerseWebApp.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+
+        public CouponApiController(AppDbContext db)
+        {
+            _db = db;
+            _response = new ResponseDto();
+        }
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = objList;
+            }
+            catch (Exception ex) 
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("id:int")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=>u.CouponId== id);
+                _response.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
